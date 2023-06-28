f6a51fd5ad04f004c7f0c17c843d1774efc3d392
XDream8 <ism.frat@hotmail.com>
2023-06-28 18:06:19 +0000

    seperate hash generation code from checksum function

--- src/checksum.rs
+++ src/checksum.rs
@@ -16,11 +16,51 @@ use blake3::Hasher;
 use std::fs::{File, OpenOptions};
 
 use std::io::{BufWriter, Read, Result, Write};
+use std::path::Path;
 
 pub fn pkg_checksum(package: &str) {
     pkg_source(package, true, false);
 
     let repo_dir = get_repo_dir();
+
+    if !Path::new(repo_dir.as_str()).join("sources").exists() {
+	return
+    }
+
+    let hashes: Vec<String> = pkg_checksum_gen(repo_dir.clone());
+
+    if !hashes.is_empty() {
+	// create or recreate checksums file
+	let checksums_file = OpenOptions::new()
+	    .write(true)
+	    .truncate(true)
+	    .create(true)
+	    .open(format!("{}/checksums", repo_dir))
+	    .expect("Failed to create or recreate checksums file");
+
+	// use a buffered writer for performance
+	let mut writer = BufWriter::new(checksums_file);
+
+	for hash in hashes {
+	    println!("{}", hash);
+	    writer
+		.write_all(hash.as_bytes())
+		.expect("Failed to write to checksums file");
+	    writer
+		.write_all(b"\n")
+		.expect("Failed to write to checksums file");
+	}
+
+	// ensure all data is written to the file
+	writer.flush().expect("Failed to write to checksums file");
+
+	log!(get_repo_name().as_str(), "Generated checksums");
+    } else {
+	log!(get_repo_name().as_str(), "No sources needing checksums");
+    }
+}
+
+pub fn pkg_checksum_gen(repo_dir: String) -> Vec<String> {
     let sources_path = format!("{}/sources", repo_dir);
     let sources: Vec<String> = read_a_files_lines(sources_path.as_str()).expect("No sources file");
 
@@ -43,44 +83,16 @@ pub fn pkg_checksum(package: &str) {
 	}
 
 	if !source.is_empty() {
-            let (res, des) = pkg_source_resolve(source, dest, false);
+	    let (res, des) = pkg_source_resolve(source, dest, false);
 
-            // if it is a local source res equals to des
-            if res == des && !res.contains("git+") {
+	    // if it is a local source res equals to des
+	    if res == des && !res.contains("git+") {
 		hashes.push(get_file_hash(&des).expect("Failed to generate checksums"));
-            }
+	    }
 	}
     }
 
-    if !hashes.is_empty() {
-	// create or recreate checksums file
-	let checksums_file = OpenOptions::new()
-            .write(true)
-            .truncate(true)
-            .create(true)
-            .open(format!("{}/checksums", repo_dir))
-            .expect("Failed to create or recreate checksums file");
-
-	// use a buffered writer for performance
-	let mut writer = BufWriter::new(checksums_file);
-
-	for hash in hashes {
-            println!("{}", hash);
-            writer
-		.write_all(hash.as_bytes())
-		.expect("Failed to write to checksums file");
-            writer
-		.write_all(b"\n")
-		.expect("Failed to write to checksums file");
-	}
-
-	// ensure all data is written to the file
-	writer.flush().expect("Failed to write to checksums file");
-
-	log!(get_repo_name().as_str(), "Generated checksums");
-    } else {
-	log!(get_repo_name().as_str(), "No sources needing checksums");
-    }
+    hashes
 }
 
 pub fn get_file_hash(file_path: &str) -> Result<String> {
@@ -94,6 +106,10 @@ pub fn get_file_hash(file_path: &str) -> Result<String> {
     hash.fill(hash_output.as_mut_slice());
 
     Ok(hex::encode(hash_output))
+}
+
+pub fn pkg_verify(pkg: &str) {
+    log!(get_repo_name(), "Verifying sources");
 }
 
 pub fn checksum_action(c: &Context) {

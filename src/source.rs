767a47c568e63509fad5cdd0d2184f488fe703c6
XDream8 <WolfsDream@proton.me>
2023-07-06 13:28:54 +0000

    pkg_source_git: almost complete the only thing missing is fetching using a specific commit

--- src/source.rs
+++ src/source.rs
@@ -8,8 +8,7 @@ use std::io::{self, Write};
 use std::path::{Path, PathBuf};
 
 use ureq::Response;
-
-use std::process::Command;
+use git2::{AutotagOption, FetchOptions, RemoteCallbacks, Repository};
 
 // logging functions
 use super::die;
@@ -151,7 +150,7 @@ pub fn pkg_source(pkg: &str, skip_git: bool, print: bool) {
 	if res != des {
 	    if !skip_git && res.starts_with("git+") {
 		// place holder
-		pkg_source_git(&repo_name, res);
+		pkg_source_git(&repo_name, res.as_str()).expect("Failed to fetch contents of repository");
 	    } else if !res.starts_with("git+") {
 		pkg_source_url(&res, Path::new(&des)).unwrap_or_else(|err| die!("Failed to download file: ", format!("{err}").as_str()));
 	    }
@@ -160,42 +159,113 @@ pub fn pkg_source(pkg: &str, skip_git: bool, print: bool) {
 }
 
 // Experimental Function to clone git repos
-// TODO: finish this function, it is currently just a placeholder
-pub fn pkg_source_git(package_name: &str, source: String) {
-    let mut com = source.clone();
-    if let Some(index) = com.find('#') {
-	com.truncate(index);
-    }
-    if let Some(index) = com.find('@') {
-	com.truncate(index);
+// https://github.com/rust-lang/git2-rs/blob/master/examples/fetch.rs
+pub fn pkg_source_git(package_name: &str, source: &str) -> Result<(), git2::Error> {
+    let repo = Repository::open(".")?;
+    let remote: &str = match source {
+	_ if !source.is_empty() => source.split("git+").last().unwrap(),
+	_ => "origin",
+    };
+
+    // Figure out whether it's a named remote or a URL
+    log!(package_name, format!("Checking out {}", remote));
+    let mut cb = RemoteCallbacks::new();
+    let mut remote = repo
+        .find_remote(remote)
+        .or_else(|_| repo.remote_anonymous(remote))?;
+    cb.sideband_progress(|data| {
+        print!("remote: {}", std::str::from_utf8(data).unwrap());
+        io::stdout().flush().unwrap();
+        true
+    });
+
+    // This callback gets called for each remote-tracking branch that gets
+    // updated. The message we output depends on whether it's a new one or an
+    // update.
+    cb.update_tips(|refname, a, b| {
+        if a.is_zero() {
+            println!("[new]     {:20} {}", b, refname);
+        } else {
+            println!("[updated] {:10}..{:10} {}", a, b, refname);
+        }
+        true
+    });
+
+    // Here we show processed and total objects in the pack and the amount of
+    // received data. Most frontends will probably want to show a percentage and
+    // the download rate.
+    cb.transfer_progress(|stats| {
+        if stats.received_objects() == stats.total_objects() {
+            print!(
+                "Resolving deltas {}/{}\r",
+                stats.indexed_deltas(),
+                stats.total_deltas()
+            );
+        } else if stats.total_objects() > 0 {
+            print!(
+                "Received {}/{} objects ({}) in {} bytes\r",
+                stats.received_objects(),
+                stats.total_objects(),
+                stats.indexed_objects(),
+                stats.received_bytes()
+            );
+        }
+        io::stdout().flush().unwrap();
+        true
+    });
+
+    // Download the packfile and index it. This function updates the amount of
+    // received data and the indexer stats which lets you inform the user about
+    // progress.
+    let mut fo = FetchOptions::new();
+    fo.remote_callbacks(cb);
+    remote.download(&[] as &[&str], Some(&mut fo))?;
+
+    {
+        // If there are local objects (we got a thin pack), then tell the user
+        // how many objects we saved from having to cross the network.
+        let stats = remote.stats();
+        if stats.local_objects() > 0 {
+            println!(
+                "\rReceived {}/{} objects in {} bytes (used {} local \
+                 objects)",
+                stats.indexed_objects(),
+                stats.total_objects(),
+                stats.received_bytes(),
+                stats.local_objects()
+            );
+        } else {
+            println!(
+                "\rReceived {}/{} objects in {} bytes",
+                stats.indexed_objects(),
+                stats.total_objects(),
+                stats.received_bytes()
+            );
+        }
     }
 
-    log!(&package_name, format!("Checking out {}",
-			       if !com.is_empty() {
-				   &com
-			       }
-			       else {
-				   "FETCH_HEAD"
-			       }).as_str()
-    );
+    // Disconnect the underlying connection to prevent from idling.
+    remote.disconnect()?;
 
-    if !Path::new(".git").exists() {
-	let _output = Command::new("git")
-	    .arg("init")
-	    .output();
-    }
+    // Update the references in the remote's namespace to point to the right
+    // commits. This may be needed even if there was no packfile to download,
+    // which can happen e.g. when the branches have been changed but all the
+    // needed objects are available locally.
+    remote.update_tips(None, true, AutotagOption::Unspecified, None)?;
+
+    Ok(())
 }
 
-pub fn pkg_source_tar(res: String, no_leading_dir: bool) {
-    let file: File = File::open(res.clone()).expect("Failed to open tar file");
-    let extension: Option<&str> = Path::new(res.as_str()).extension().and_then(|ext| ext.to_str());
-    let mut decoder: Box<dyn Read> = match extension {
-	Some("gz") => Box::new(GzDecoder::new(file)),
-	Some("xz") => Box::new(XzDecoder::new(file)),
-	Some("bz2") => Box::new(BzDecoder::new(file)),
-	Some("zst") => Box::new(Decoder::new(file).expect("Failed to decompress tar.zst archive")),
-	_ => return,
-    };
+    pub fn pkg_source_tar(res: String, no_leading_dir: bool) {
+	let file: File = File::open(res.clone()).expect("Failed to open tar file");
+	let extension: Option<&str> = Path::new(res.as_str()).extension().and_then(|ext| ext.to_str());
+	let mut decoder: Box<dyn Read> = match extension {
+	    Some("gz") => Box::new(GzDecoder::new(file)),
+	    Some("xz") => Box::new(XzDecoder::new(file)),
+	    Some("bz2") => Box::new(BzDecoder::new(file)),
+	    Some("zst") => Box::new(Decoder::new(file).expect("Failed to decompress tar.zst archive")),
+	    _ => return,
+	};
 
     let mut archive: Archive<&mut Box<dyn std::io::Read>> = Archive::new(&mut decoder);
 
